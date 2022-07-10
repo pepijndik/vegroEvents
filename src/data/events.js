7fa9062993440441861c123b57487c4916c9a64c
Pepijn dik <pepijn@pdik.nl>
2022-07-10 19:43:44 +0200

    Added some events

--- src/data/events.js
+++ src/data/events.js
@@ -3,7 +3,7 @@ let events = [
   {
     id: 1,
     title: "Larafest",
-    description: "Every thing about the newest version",
+    description: "Every thing about the newest version of Laravel, will be discussing the newest invoke albes and more",
     start: "2022-07-08T12:00:00",
     end: "2022-07-08T15:35",
     location: "lisse",
@@ -19,17 +19,26 @@ let events = [
           },
         ],
       },
+      {
+        name: "Marcel de groot",
+        socials: [
+          {
+            type: "twitter",
+            url: "/MarceldGroot",
+          },
+        ],
+      },
     ],
     max_attendees: 25,
     type: "Meetup",
   },
   {
     id: 2,
-    title: "Test",
-    description: "A test event",
-    start: "2022-07-10T10:00:00",
-    end: "2022-07-10T15:35",
-    location: "Noordwijk",
+    title: "Vegrobotica",
+    description: "Discussing the creations of a new inovation at vegro",
+    start: "2022-07-12T10:00:00",
+    end: "2022-07-10T10:35",
+    location: "Katwijk",
     image:
       "https://www.codingem.com/wp-content/uploads/2021/10/juanjo-jaramillo-mZnx9429i94-unsplash-1024x683.jpg",
     speakers: [
@@ -42,9 +51,66 @@ let events = [
           },
         ],
       },
+      {
+        name: "Maurice de Jongh",
+        socials: [
+          {
+            type: "facebook",
+            url: "/Maurice",
+          },
+        ],
+      },
     ],
     max_attendees: 2,
     type: "Virtual",
+  },
+  {
+    id: 3,
+    title: "React",
+    description: "Who is building the nex big react frameworks? lets disguss",
+    start: "2022-09-25T12:54:00",
+    end: "2022-09-01T15:35",
+    location: "Leiden",
+    image:
+      "https://www.codingem.com/wp-content/uploads/2021/10/juanjo-jaramillo-mZnx9429i94-unsplash-1024x683.jpg",
+    speakers: [
+   
+      {
+        name: "Maurice de Jongh",
+        socials: [
+          {
+            type: "facebook",
+            url: "/Maurice",
+          },
+        ],
+      },
+    ],
+    max_attendees: 2,
+    type: "Location",
+  },
+  {
+    id: 4,
+    title: "wwdc22",
+    description: "Introducing the new MacBook Air, 13‑inch MacBook Pro, iOS 16, iPadOS 16, macOS Ventura, and watchOS 9",
+    start: "2022-09-01T09:00:00",
+    end: "2022-09-01T15:35",
+    location: "Online",
+    image:
+      "https://www.apple.com/v/apple-events/home/w/images/overview/hero__d6adldydsqye_xlarge.png",
+    speakers: [
+   
+      {
+        name: "Maurice de Jongh",
+        socials: [
+          {
+            type: "facebook",
+            url: "/Maurice",
+          },
+        ],
+      },
+    ],
+    max_attendees: 2,
+    type: "Location",
   },
 ];
 
